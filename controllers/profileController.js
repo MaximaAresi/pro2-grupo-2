e7360f6bd1a8c12ad382ca93cb584b5dd69f437d
Alejandro Villar <villars@udesa.edu.ar>
2024-04-15 11:22:40 -0300

    Completé cabecera de 'Mi Perfil' y el usuario en headerLogueado con datos traidos desde el módulo de datos

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,6 +1,8 @@
+const db =  require('../db/info')
+
 let profileController = {
     main: function(req, res) {
-        return res.render("profile");
+        return res.render("profile", {db: db});
     },
     edit: function(req, res) {
         return res.render("profile-edit");
