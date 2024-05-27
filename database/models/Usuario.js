b46b172d6e2f80564e5330c6479790ac87e15d19
Alejandro Villar <villars@udesa.edu.ar>
2024-05-27 11:56:55 -0300

    cree modelo de usuarios

---
+++ database/models/Usuario.js
@@ -0,0 +1,44 @@
+module.exports = function (sequelize, dataTypes) {
+	let alias = "Usuario"; 
+    let cols = { 
+        id: {
+            autoIncrement: true,
+            primaryKey: true, 
+            type: dataTypes.INTEGER
+            },
+        usuario:{
+            type: dataTypes.STRING
+            },
+        mail: {
+            type: dataTypes.STRING
+            },
+        contrasenia:{
+            type: dataTypes.STRING
+            },
+        fecha: {
+            type: dataTypes.DATE
+            },
+        DNI:{
+            type: dataTypes.INTEGER
+            },
+        fotoPerfil: {
+            type: dataTypes.STRING
+            },
+        ceratedAt: {
+            type: dataTypes.DATE
+            },
+        updatedAt: {
+            type: dataTypes.DATE
+            },
+        deletedAt: {
+            type: dataTypes.DATE
+            }     
+    }
+    let config = {
+        tableName: "usuarios", 
+        timestamps: false, 
+        underscore: false
+    }
+    let Usuario = sequelize.define(alias, cols, config);
+    return Usuario;
+}
